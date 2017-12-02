8ed72a0bd2808c18e5044e7438cf38edd85978b8
daniyalahmedkhan <daniyalahmed520@gmail.com>
2017-12-03 02:22:54 +0500

    Login Form

---
+++ app/src/main/java/com/example/kashif/bookphotographer/Activities/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.kashif.bookphotographer.Activities;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.kashif.bookphotographer.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = this.getWindow();
+        window.setBackgroundDrawableResource(R.drawable.bck);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+
+
+        setContentView(R.layout.activity_login);
+    }
+}
